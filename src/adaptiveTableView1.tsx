0fcc8053613fedd5e419b01d02aa4b4047d2d887
ashaban <AShaban@innovationteam.com>
2025-05-11 16:16:36 +0300

    feat: table view

---
+++ src/adaptiveTableView1.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, useRef } from 'react';
+import {
+  requireNativeComponent,
+  UIManager,
+  View,
+  Platform,
+  findNodeHandle,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+
+type Props = {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+  onEndReached: () => void;
+};
+
+const ComponentName = 'AdaptiveTableView1';
+
+const NativeComponent = requireNativeComponent<Props>(ComponentName);
+
+export default function AdaptiveTableView1({
+  children,
+  style,
+  onEndReached,
+}: Props) {
+  return (
+    <NativeComponent style={style} onEndReached={onEndReached}>
+      {React.Children.map(children, (child, index) => (
+        <View collapsable={false} key={index}>
+          {child}
+        </View>
+      ))}
+    </NativeComponent>
+  );
+}
